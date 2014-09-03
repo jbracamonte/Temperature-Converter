9e63c283dbbbf1f8c63b02cff4b92798e3c4879c
Jawaharlal Bracamonte <jbracamonte@MacBook-Pro.local>
2014-09-03 23:11:13 +0800

    Initial Commit

---
+++ Temperature Converter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Temperature Converter
+//
+//  Created by Jawaharlal Bracamonte on 9/3/14.
+//  Copyright (c) 2014 Barcode Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Temperature Converter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Temperature Converter
+//
+//  Created by Jawaharlal Bracamonte on 9/3/14.
+//  Copyright (c) 2014 Barcode Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
